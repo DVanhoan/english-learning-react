6fe160e29feb748ed8f428d00f6ddb11010df61b
DVanhoan <144916089+DVanhoan@users.noreply.github.com>
2025-12-14 23:31:02 +0700

    ddd

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,56 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    nodePolyfills({
+      include: ['events', 'stream', 'util', 'process', 'buffer'],
+      globals: {
+        Buffer: true,
+        global: true,
+        process: true,
+      },
+      protocolImports: true,
+    }),
+  ],
   server: {
     port: 3000,
   },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      util: 'rollup-plugin-node-polyfills/polyfills/util',
+      sys: 'util',
+      events: 'rollup-plugin-node-polyfills/polyfills/events',
+      stream: 'rollup-plugin-node-polyfills/polyfills/stream',
+      path: 'rollup-plugin-node-polyfills/polyfills/path',
+      querystring: 'rollup-plugin-node-polyfills/polyfills/qs',
+      punycode: 'rollup-plugin-node-polyfills/polyfills/punycode',
+      url: 'rollup-plugin-node-polyfills/polyfills/url',
+      string_decoder: 'rollup-plugin-node-polyfills/polyfills/string-decoder',
+      http: 'rollup-plugin-node-polyfills/polyfills/http',
+      https: 'rollup-plugin-node-polyfills/polyfills/http',
+      os: 'rollup-plugin-node-polyfills/polyfills/os',
+      assert: 'rollup-plugin-node-polyfills/polyfills/assert',
+      constants: 'rollup-plugin-node-polyfills/polyfills/constants',
+      _stream_duplex: 'rollup-plugin-node-polyfills/polyfills/stream/duplex',
+      _stream_passthrough: 'rollup-plugin-node-polyfills/polyfills/stream/passthrough',
+      _stream_readable: 'rollup-plugin-node-polyfills/polyfills/stream/readable',
+      _stream_writable: 'rollup-plugin-node-polyfills/polyfills/stream/writable',
+      _stream_transform: 'rollup-plugin-node-polyfills/polyfills/stream/transform',
+      timers: 'rollup-plugin-node-polyfills/polyfills/timers',
+      console: 'rollup-plugin-node-polyfills/polyfills/console',
+      vm: 'rollup-plugin-node-polyfills/polyfills/vm',
+      zlib: 'rollup-plugin-node-polyfills/polyfills/zlib',
+      tty: 'rollup-plugin-node-polyfills/polyfills/tty',
+      domain: 'rollup-plugin-node-polyfills/polyfills/domain'
     },
+  },
+  define: {
+    global: 'window',
   },
 });
